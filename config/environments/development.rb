265f45d9dba34a2f861f13a62a8066b2422bbee6
Bang Nguyen <bangnv77@yahoo.com>
2016-09-28 17:40:31 -0700

    create User model through Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,5 +53,16 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Needed for Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "your_email@gmail.com",
+      password: "your_password"
+
+  }
 end
